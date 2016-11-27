11fff60ddbb2fec822c20599ee0116f43694a707
Jennifer Wong <wongjenn@yahoo.com>
2016-11-27 15:56:58 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+#  if num < 11
+    while num > 0 do
+      puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+#  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  while num > 0 do
+    sleep(1)
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
